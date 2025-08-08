51784c70993cf56fc0e186b3e8ed51db085d1e83
Lolp3300 <129900910+Lolp3300@users.noreply.github.com>
2025-08-08 01:13:04 -0400

    Delete quest.js

--- quest.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.zoomable').forEach(img => {
-    img.addEventListener('click', () => {
-      // Toggle le zoom en ajoutant/retirant la classe "zoomed"
-      img.classList.toggle('zoomed');
-    });
-  });
